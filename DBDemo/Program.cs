fc1a43c3f2e5aa33fae46b21f881f110ed643c6c
David <david.wang@mail.kway.com.tw>
2024-10-29 17:34:39 +0800

    修改註解

--- DBDemo/Program.cs
+++ DBDemo/Program.cs
@@ -13,30 +13,36 @@ namespace DBDemo
     {
         static void Main(string[] args)
         {
+            //Version=3 => SQLite版本為3
             string connectionString = "Data Source=database.db;Version=3;";
 
+            //使用using確保SQLiteConnection在使用後會自動關閉並釋放資源
+            //var connection = new SQLiteConnection(connectionString) =>與SQLite資料庫建立連線
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
 
-                // 查詢非管理職員工
-                var nonManagerialEmployees = connection.Query<string>("SELECT name FROM Employees WHERE managerId IS NOT NULL");
+                // 查詢非管理職員工(managerId != nulla)
+                //使用 Dapper 的 Query 方法執行 SQL 查詢並返回結果。Query<string>代表傳回的質會用string格式呈現
+                var managerIdEmployees = connection.Query<string>("SELECT name FROM Employees WHERE managerId IS NOT NULL");
 
-                Console.WriteLine("非管理職員工：");
-                foreach (var employee in nonManagerialEmployees)
+                Console.WriteLine("非管理職員工(managerId != NULL)：");
+                foreach (var employee in managerIdEmployees)
                 {
                     Console.WriteLine(employee);
                 }
                 Console.ReadLine();
             }
 
+            //重新使用using，每個查詢都需要新的資料庫連接
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
 
                 // 查詢非管理職且薪資高於其主管的員工
+                //
                 var result = connection.Query<string>(@"
-                SELECT e1.name AS Employee
+                SELECT e1.name 
                 FROM Employees e1
                 JOIN Employees e2 ON e1.managerId = e2.id
                 WHERE e1.salary > e2.salary
