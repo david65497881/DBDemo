2bd220d241e651f45e98a20ae1698779fbb820f1
David <david.wang@mail.kway.com.tw>
2024-10-29 17:06:05 +0800

    建立SEARCH2分支

--- DBDemo/Program.cs
+++ DBDemo/Program.cs
@@ -15,21 +15,20 @@ namespace DBDemo
         {
             string connectionString = "Data Source=database.db;Version=3;";
 
-            // 檢查資料庫檔案是否存在；如果不存在，則建立
-            if (!File.Exists("./database.db"))
-            {
-                SQLiteConnection.CreateFile("database.db");
-            }
-
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
 
-                // 查詢非管理職員工
-                var nonManagerialEmployees = connection.Query<string>("SELECT name FROM Employees WHERE managerId IS NOT NULL");
+                // 查詢非管理職且薪資高於其主管的員工
+                var result = connection.Query<string>(@"
+                SELECT e1.name AS Employee
+                FROM Employees e1
+                JOIN Employees e2 ON e1.managerId = e2.id
+                WHERE e1.salary > e2.salary
+            ");
 
-                Console.WriteLine("非管理職員工：");
-                foreach (var employee in nonManagerialEmployees)
+                Console.WriteLine("非管理職且薪資高於其主管的員工：");
+                foreach (var employee in result)
                 {
                     Console.WriteLine(employee);
                 }
